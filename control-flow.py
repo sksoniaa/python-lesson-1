a8cd8a7825594ef84a6ee7537a655e3dfe1b8b0e
Sofiia Kosachevych <sofiakosachvych@gmail.com>
2024-03-26 12:49:33 -0500

    python lesson-1-sterter

---
+++ control-flow.py
@@ -0,0 +1,9 @@
+
+
+age = int(input("What is your age?"))
+print(f"My age is {age}")
+
+if age >= 21:
+  print("You can have a drink")
+else:
+  print("You are too young")
---
+++ intro-python.py
@@ -0,0 +1,18 @@
+# the hashtag is the comment syntax
+# print is the console.log of python
+print("hello world")
+
+#snake_case as the convention for naming variables
+total_guitar = 7 #defining a variable
+print(total_guitar)
+
+total_guitar = 8
+print(total_guitar)
+
+
+print(type(total_guitar)) #int
+print(type("hello")) 
+print(type(4.12))#float
+
+
+print(4.0 + 5)
